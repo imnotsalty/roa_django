59005fca0e86fc2242c6e756b040dc0387418f5b
imnotsaltyBTW <studiosaltilicious@gmail.com>
2025-07-03 14:40:17 +0530

    Repo creation

---
+++ apps/common/models/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from .config import (
+    COMMON_BLANK_AND_NULLABLE_FIELD_CONFIG,
+    COMMON_CHAR_FIELD_MAX_LENGTH,
+    COMMON_NULLABLE_FIELD_CONFIG,
+)
+from .base import BaseModel
